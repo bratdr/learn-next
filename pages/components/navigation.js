fc713f1bb6cf3cdae9bc503006cedf0d22a2b823
Brata Darmawan <bratadarmawan@gmail.com>
2023-01-07 04:45:38 +0700

    Update navigation.js

--- pages/components/navigation.js
+++ pages/components/navigation.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Link from "next/link";
 
 const navigation = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      {/* Navigation bar */}
+      <div className="flex flex-row justify-between items-center bg-black px-8 text-slate-50 h-20">
+        <Link href="/">Learn Next.js</Link>
+        <ul className="flex flex-row gap-6">
+          <li>
+            {/* using next/link feature from next.js */}
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default navigation;
